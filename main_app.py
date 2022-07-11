a896320ea72b6e1d8421e8365d216fcc1d034e41
Eduarda Chagas <eduardamachagas@gmail.com>
2022-07-10 22:58:52 -0300

    Ajustes

--- main_app.py
+++ main_app.py
@@ -5,19 +5,17 @@ import pandas as pd
 import time
 
 from streamlit.type_util import data_frame_to_bytes
-from acervo import acervo
+from recipe_book import book
 
 
 st.title('Busca em Receitas da Vovó Duds')
 DATE_COLUMN = 'date/time'
 
-pesquisa = acervo()
-total = st.write('Acervo = ', pesquisa.quant_docs)
+pesquisa = book()
+total = st.write('Livro da vovó Duds contém', pesquisa.quant_docs, 'receitas.')
 with st.form(key='my_form'):
     text_input = st.text_input(label='Vamos almoçar?')
     search_button = st.form_submit_button(label='Arroz soltinho')
-
-    print(text_input)
     
 if search_button:
     doc_DIR = pesquisa.doc_DIR
@@ -45,21 +43,20 @@ if search_button:
                     vet_simi.append(simil)
                 receitas_encontradas = receitas_encontradas+1
                 latest_iteration.text(f'Receitas relevantes: {cont+1}')
-                if cont != 90:
-                    cont = cont+1
-                if cont2 == (pesquisa.quant_docs - 2):
-                    cont2 = cont2+1
-                print(recipes)
+            if cont != 90:
+                cont = cont+1
+            if cont2 == (pesquisa.quant_docs - 2):
+                cont2 = cont2+1
     if idf != 0:
         quant_docs = pesquisa.qtdeDocsComTermo(text_input)
         st.write("Receitas encontradas: ",quant_docs)
         result2 = result
         cont = 0
         vetores = ["0","1","2","3","4"]
-        for x in range(0,len(result2)):
+        for x in range(0,len(result2)): 
             with st.form(key='my-form2' + vetores[x]):
-                name, id, minutes, contributorid, submitted, tags, nutrition, nsteps, steps, description, ingredients = pesquisa.printDoc(result2[x])
-                
+                name, id, minutes, contributorid, submitted, tags, nutrition, nsteps, steps, description, ingredients = pesquisa.printRecipe(result2[x])
+
                 st.write("Similaridade: ",vet_simi[x],"\n\n Nome: " + name, "\n Id da receita: " + id, "\n Id do contribuidor: " + contributorid, "\n Data de submissão: " + submitted, 
                 "\n Palavras-chave: " + tags, "\n Valor nutricional: " + nutrition, "\n Número de passos: " + nsteps, "\n Passos: " + steps, "\n Descrição: " + description)
                 
@@ -68,5 +65,3 @@ if search_button:
                     st.text("Cancelar busca, não estou mais com fome")
                 if cont == 4:
                     cont = cont + 1
-
-        print(text_input)
